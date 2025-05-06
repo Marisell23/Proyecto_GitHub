df016c516cee73b3b0dd5f1834380301dcdd236b
Marisell23 <stephanymarissellgarciavallada@gmail.com>
2025-05-06 15:01:44 -0600

    Console.ReadKey();

--- Proyecto_GitHub/Program.cs
+++ Proyecto_GitHub/Program.cs
@@ -11,6 +11,7 @@ namespace Proyecto_GitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Mi primer proyecto de GitHub.");
+            console.ReadKey();
         }
     }
 }
